9290c4626595e71341083a65d1f015bd6f548848
lkb0398 <lkb0398@nate.com>
2021-10-23 06:13:33 -0400

    udate db.js URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = {
 
-    "URI":"mongodb://localhost/book_store",
+    // "URI":"mongodb://localhost/book_store",
+    "URI":"mongodb+srv://kyeongbeom:Y4aDfh5IQbeAA59F@mongodbserver.oo53d.mongodb.net/book_store?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
 };
